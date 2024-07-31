f0e826b53287c1f002bfddec153d1bf5b192803a
CihanErdem <cihaneerdemdiker55@gmail.com>
2024-07-31 10:53:17 +0300

    bişeyler

--- src/forms/btn/btn.component.ts
+++ src/forms/btn/btn.component.ts
@@ -1,18 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output,  } from '@angular/core';
 
 @Component({
   selector: 'app-btn',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './btn.component.html',
   styleUrl: './btn.component.css'
 })
 export class BtnComponent {
-  @Input() label: string = ''; 
-  @Output() buton = new EventEmitter<void>();
+  @Input() label: string | undefined;
+  @Input() buttonClass: string | undefined;
+  @Output() buttonClick = new EventEmitter<void>();
 
   onClick() {
-    this.buton.emit();
+    this.buttonClick.emit();
   }
 
   
